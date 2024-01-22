007e4da5b1654e353d23d40b77a3773d09725161
Vítor Oliveira <vitorsk807@gmail.com>
2024-01-22 00:16:50 -0300

    g

--- Generator.py
+++ Generator.py
@@ -1,9 +1,10 @@
 import secrets
-import string
+import string 
 
 intens = (string.ascii_letters + string.digits)
 
 senha = [secrets.choice(intens)
          for i in range(15)]
 senha = ''.join(senha)
-print(senha) 
+print(senha)
+
