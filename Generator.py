f49df9ff5e639b99deac579c1e1661a677229c78
Vítor Oliveira <vitorsk807@gmail.com>
2024-01-29 00:20:14 -0300

    Configurando código para ser impresso no bloco de notas

--- Generator.py
+++ Generator.py
@@ -1,9 +1,25 @@
-import secrets
-import string 
- 
-intens = (string.ascii_letters + string.digits)
+# import secrets
+# import string
 
-senha = [secrets.choice(intens)
-         for i in range(15)]
-senha = ''.join(senha)
-print(senha)
+# intens = (string.ascii_letters + string.digits)
+
+# senha = [secrets.choice(intens)
+#          for i in range(15)]
+# senha = ''.join(senha)
+# print(senha)
+
+# Pressionar botão windows
+# Ecrver Bloco de notas 
+# Pressionar Enter
+
+import pyautogui
+import time 
+#import secrets
+#import string
+
+pyautogui.press("win")
+time.sleep()
+pyautogui.write("Bloco de notas")
+time.sleep(2)
+pyautogui.press("enter")
+
